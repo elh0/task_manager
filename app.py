a71d64b426b932719414e01c564b948ead4277b3
Elliot <elliotholbrow@gmail.com>
2019-07-24 17:14:08 +0100

    Attempted to run locally

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def get_tasks():
 
 
 if __name__ == '__main__':
-            app.run(host=os.getenv("IP", "0.0.0.0"), port=int(
-                os.getenv("PORT", "5000")), debug=True)
+
+    app.run()
